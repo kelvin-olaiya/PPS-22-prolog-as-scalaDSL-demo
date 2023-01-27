901f6fa72f566047e321cc1aff5a4ff2e7f99b1b
Jahrim Gabriele Cesario <jahrim.cesario2@studio.unibo.it>
2023-01-27 13:16:37 +0100

    chore: added a main class and an example test, also refactored the dependencies through the default gradle catalog

---
+++ app/src/main/scala/io/github/chess/Main.scala
@@ -0,0 +1,8 @@
+package io.github.chess
+
+/**
+ * The main application.
+ */
+object Main extends App:
+  def greeting: String = "Hello, world!"
+  println(greeting)
---
+++ app/src/test/scala/io/github/chess/TemplateTest.scala
@@ -0,0 +1,15 @@
+package io.github.chess
+
+import org.scalatest.funspec.AnyFunSpec
+
+/**
+ * A template for writing tests with scala test.
+ */
+class TemplateTest extends AnyFunSpec:
+  describe("This test") {
+    describe("when run") {
+      it("should never fail") {
+        assert(true)
+      }
+    }
+  }
