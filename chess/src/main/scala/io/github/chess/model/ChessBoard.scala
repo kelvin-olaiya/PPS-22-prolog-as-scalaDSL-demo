5eccff49a8e6282d7e852a394be51f15533ba714
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2023-02-08 16:20:23 +0100

    feat: add chess board factory and Position to Move mapping.

--- chess/src/main/scala/io/github/chess/model/ChessBoard.scala
+++ chess/src/main/scala/io/github/chess/model/ChessBoard.scala
@@ -12,61 +12,66 @@ import io.github.chess.model.Team.{BLACK, WHITE}
 trait ChessBoard:
 
   /**
-   * Gives access to all the pieces that are present on the board.
-   * @return the map containing both white and black pieces of the board
+   * Gives access to all the [[Piece]]s that are present on the board.
+   * @return the map containing both white and black [[Piece]]s of the board
    */
   def pieces: Map[Position, Piece]
 
   /**
    * Gives all the available moves for a pieces placed in a specified position.
-   * @param position the position where the piece to be moved is placed
+   * @param position the [[Position]] where the piece to be moved is placed
    * @return all the available moves that could be performed by the piece
    */
   def findMoves(position: Position): Set[Move]
 
   /**
    * Performs the move by a piece on the board.
-   * @param move The move to be executed
+   * @param move The [[Move]] to be executed
    */
   def move(move: Move): Unit
 
-/** The implementation of the Chess Board. */
-case class ChessBoardImpl() extends ChessBoard:
-  import scala.collection.immutable.HashMap
+/** Factory for [[ChessBoard]] instances. */
+object ChessBoard:
 
-  private var whitePieces: Map[Position, Piece] = HashMap()
-  private var blackPieces: Map[Position, Piece] = HashMap()
-  private var currentlyPlayingTeam = Team.WHITE
+  /**
+   * Creates a new Chess Board.
+   * @return a new [[ChessBoard]]
+   */
+  def apply(): ChessBoard = ChessBoardImpl()
 
-  override def pieces: Map[Position, Piece] = this.whitePieces ++ this.blackPieces
+  private case class ChessBoardImpl() extends ChessBoard:
+    import scala.collection.immutable.HashMap
 
-  override def findMoves(position: Position): Set[Move] =
-    val team = playingTeam
-    val selectedPiece = team.get(position)
-    selectedPiece match
-      case Some(piece) =>
-        val destinations = piece.findMoves(position)
-      // destinations.map(_ => new Move(position, _))
-      // Or map it in a different way, the destination positions to the actual move of the piece
-    Set.empty
+    private var whitePieces: Map[Position, Piece] = HashMap()
+    private var blackPieces: Map[Position, Piece] = HashMap()
+    private var currentlyPlayingTeam = Team.WHITE
 
-  override def move(move: Move): Unit =
-    val newTeam = this.applyMove(move)
-    this.currentlyPlayingTeam match
-      case WHITE => this.whitePieces = newTeam
-      case BLACK => this.blackPieces = newTeam
-    changePlayingTeam()
+    override def pieces: Map[Position, Piece] = this.whitePieces ++ this.blackPieces
 
-  private def playingTeam: Map[Position, Piece] = this.currentlyPlayingTeam match
-    case WHITE => this.whitePieces
-    case BLACK => this.blackPieces
+    override def findMoves(position: Position): Set[Move] =
+      val team = playingTeam
+      val selectedPiece = team.get(position)
+      selectedPiece match
+        case Some(piece) => piece.findMoves(position).map(dest => Move(position, dest))
+        case None        => Set.empty
 
-  private def changePlayingTeam(): Unit = this.currentlyPlayingTeam =
-    this.currentlyPlayingTeam.oppositeTeam
+    override def move(move: Move): Unit =
+      val newTeam = this.applyMove(move)
+      this.currentlyPlayingTeam match
+        case WHITE => this.whitePieces = newTeam
+        case BLACK => this.blackPieces = newTeam
+      changePlayingTeam()
 
-  private def applyMove(move: Move): Map[Position, Piece] =
-    val team = playingTeam
-    val pieceToMove = team.get(move.from)
-    pieceToMove match
-      case Some(piece) => team - move.from + ((move.to, piece))
-      case None        => team
+    private def playingTeam: Map[Position, Piece] = this.currentlyPlayingTeam match
+      case WHITE => this.whitePieces
+      case BLACK => this.blackPieces
+
+    private def changePlayingTeam(): Unit = this.currentlyPlayingTeam =
+      this.currentlyPlayingTeam.oppositeTeam
+
+    private def applyMove(move: Move): Map[Position, Piece] =
+      val team = playingTeam
+      val pieceToMove = team.get(move.from)
+      pieceToMove match
+        case Some(piece) => team - move.from + ((move.to, piece))
+        case None        => team
