6a0f937c5d6be830c9b577defceef6500ccc4fbd
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2023-02-27 09:57:30 +0100

    fix: solve pawns not being able to capture on File H

---
+++ chess/src/main/scala/io/github/chess/model/rules/prolog/InsideBoardFilterRule.scala
@@ -0,0 +1,19 @@
+/*
+ * MIT License
+ * Copyright (c) 2023 Cesario Jahrim Gabriele & Derevyanchenko Maxim & Felice Mirko & Kentpayeva Madina
+ *
+ * Full license description available at: https://github.com/jahrim/PPS-22-chess/blob/master/LICENSE
+ */
+package io.github.chess.model.rules.prolog
+
+import io.github.chess.model.Position
+
+/**
+ * Represents an alternative rule to [[InsideBoardRule]] rule that limits
+ * the positions inside the board by filtering.
+ */
+trait InsideBoardFilterRule(private val maxX: Int = 7, private val maxY: Int = 7)
+    extends PrologRule:
+
+  abstract override def findPositions(position: Position): LazyList[(Int, Int)] =
+    super.findPositions(position).filter((x, y) => x >= 0 && x <= maxX && y >= 0 && y <= maxY)
