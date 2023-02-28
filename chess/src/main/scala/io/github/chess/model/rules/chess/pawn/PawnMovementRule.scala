fe9fd67f8528a6eee48f84cd1e471a579d67be57
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2023-02-28 11:28:57 +0100

    fix: improve first move check of the pawn

--- chess/src/main/scala/io/github/chess/model/rules/chess/pawn/PawnMovementRule.scala
+++ chess/src/main/scala/io/github/chess/model/rules/chess/pawn/PawnMovementRule.scala
@@ -6,18 +6,26 @@
  */
 package io.github.chess.model.rules.chess.pawn
 
-import io.github.chess.model.{Position, ChessGameStatus}
+import io.github.chess.model.{ChessGameStatus, Position, Rank, Team}
 import io.github.chess.model.rules.chess.{ChessRule, RuleShorthands}
 import io.github.chess.model.moves.Move
 import PawnMovementRule.*
+import io.github.chess.model.pieces.Pawn
 
 /** Set of Pawn movement rules. */
 class PawnMovementRule extends ChessRule with RuleShorthands:
   override def findMoves(position: Position, status: ChessGameStatus): Set[Move] =
     val firstStep = forwardOneRule.findMoves(position, status)
-    if firstStep.nonEmpty && isFirstMovementOf(position)(using status) then
+    if firstStep.nonEmpty && isFirstMove(position, status) then
       firstStep ++ doubleMoveRule.findMoves(position, status)
     else firstStep
+
+  private def isFirstMove(position: Position, status: ChessGameStatus): Boolean =
+    isFirstMovementOf(position)(using status) && (pieceAt(position)(using status) match
+      case Some(pawn: Pawn) if pawn.team == Team.WHITE => position.rank == Rank._2
+      case Some(pawn: Pawn) if pawn.team == Team.BLACK => position.rank == Rank._7
+      case _                                           => false
+    )
 
 /** Companion object of [[PawnMovementRule]]. */
 object PawnMovementRule:
