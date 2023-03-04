3d4add3666471eda96d795c05c5caf91e3f3e762
Mirko Felice <mirko.felice@studio.unibo.it>
2023-03-04 16:51:14 +0100

    refactor: change name of Combinator function

--- chess/src/main/scala/io/github/chess/util/general/Combinator.scala
+++ chess/src/main/scala/io/github/chess/util/general/Combinator.scala
@@ -10,13 +10,13 @@ package io.github.chess.util.general
 object Combinator:
 
   /**
-   * Generate a set of positions, starting from a specific couple of coordinates and combining it with the values as X and Y.
+   * Generate a set of coordinates, starting from a specific couple of coordinates and combining it with the values as X and Y.
    * @param values values to combine
    * @param position starting position as a couple of coordinates
    * @param condition condition used to filter the combinations, default to true
-   * @return
+   * @return a [[Set]] of coordinates (Int, Int)
    */
-  def generatePositions(
+  def generateCoordinates(
       values: Set[Int],
       position: (Int, Int),
       condition: (Int, Int) => Boolean = (_, _) => true
