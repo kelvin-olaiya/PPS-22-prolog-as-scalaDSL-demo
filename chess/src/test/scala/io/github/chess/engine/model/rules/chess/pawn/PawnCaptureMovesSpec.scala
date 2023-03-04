a6a67c8931198e65673eeac3eccbcff62b88a0b5
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2023-03-04 17:50:04 +0100

    test: add testing of pawn capturing moves

---
+++ chess/src/test/scala/io/github/chess/engine/model/rules/chess/pawn/PawnCaptureMovesSpec.scala
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ * Copyright (c) 2023 Cesario Jahrim Gabriele & Derevyanchenko Maxim & Felice Mirko & Kentpayeva Madina
+ *
+ * Full license description available at: https://github.com/jahrim/PPS-22-chess/blob/master/LICENSE
+ */
+package io.github.chess.engine.model.rules.chess.pawn
+
+import io.github.chess.engine.model.game.Team
+import io.github.chess.engine.model.pieces.Pawn
+
+/** Test suit for the rule that finds the possible capture moves of a [[io.github.chess.model.pieces.Pawn]]. */
+class PawnCaptureMovesSpec extends AbstractPawnSpec:
+
+  "PawnCaptureMoves rule" should "always find at least one move for white pawns" in {
+    testAllPositionsCaptureFor(whitePawn)
+  }
+
+  it should "always find at least one move for black pawns" in {
+    testAllPositionsCaptureFor(blackPawn)
+  }
+
+  private def testAllPositionsCaptureFor(pawn: Pawn): Unit =
+    pawnPositions.foreach(pawnPosition => {
+      addPiece(pawnPosition, pawn)
+      val (capturingPositionLeft, capturingPositionRight) = pawn.team match
+        case Team.WHITE =>
+          (pawnPosition.fileBackward().rankUp(), pawnPosition.fileForward().rankUp())
+        case Team.BLACK =>
+          (pawnPosition.fileBackward().rankDown(), pawnPosition.fileForward().rankDown())
+
+      val foundMoves = PawnCaptureMoves().findMoves(pawnPosition, status)
+      println(foundMoves)
+      foundMoves.map(_.to) should contain atLeastOneOf (
+        capturingPositionLeft,
+        capturingPositionRight
+      )
+      cleanBoard()
+    })
