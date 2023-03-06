ca0ce7fefb946ba0f1c8830d2ba88d5832f06a18
Mirko Felice <mirko.felice@studio.unibo.it>
2023-03-06 11:44:36 +0100

    chore: fix typo variable

--- chess/src/main/scala/io/github/chess/util/general/Combinator.scala
+++ chess/src/main/scala/io/github/chess/util/general/Combinator.scala
@@ -12,17 +12,17 @@ object Combinator:
   /**
    * Generate a set of coordinates, starting from a specific couple of coordinates and combining it with the values as X and Y.
    * @param values values to combine
-   * @param position starting position as a couple of coordinates
+   * @param initialCoords initial coordinates a couple of (Int, Int)
    * @param condition condition used to filter the combinations, default to true
    * @return a [[Set]] of coordinates (Int, Int)
    */
   def generateCoordinates(
       values: Set[Int],
-      position: (Int, Int),
+      initialCoords: (Int, Int),
       condition: (Int, Int) => Boolean = (_, _) => true
   ): Set[(Int, Int)] =
     for {
       x <- values
       y <- values
       if condition(x, y)
-    } yield (position._1 + x, position._2 + y)
+    } yield (initialCoords._1 + x, initialCoords._2 + y)
