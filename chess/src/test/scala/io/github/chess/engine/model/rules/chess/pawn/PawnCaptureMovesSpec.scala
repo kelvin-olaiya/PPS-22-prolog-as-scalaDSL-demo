271a226de1ec779d4ac31fa554d9e519017cbf76
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2023-03-06 13:40:32 +0100

    fix: avoid going outside the board limits in Pawn Capture Spec

--- chess/src/test/scala/io/github/chess/engine/model/rules/chess/pawn/PawnCaptureMovesSpec.scala
+++ chess/src/test/scala/io/github/chess/engine/model/rules/chess/pawn/PawnCaptureMovesSpec.scala
@@ -6,6 +6,7 @@
  */
 package io.github.chess.engine.model.rules.chess.pawn
 
+import io.github.chess.engine.model.board.{File, Position}
 import io.github.chess.engine.model.game.Team
 import io.github.chess.engine.model.pieces.Pawn
 
@@ -23,17 +24,25 @@ class PawnCaptureMovesSpec extends AbstractPawnSpec:
   private def testAllPositionsCaptureFor(pawn: Pawn): Unit =
     pawnPositions.foreach(pawnPosition => {
       addPiece(pawnPosition, pawn)
-      val (capturingPositionLeft, capturingPositionRight) = pawn.team match
-        case Team.WHITE =>
-          (pawnPosition.fileBackward().rankUp(), pawnPosition.fileForward().rankUp())
-        case Team.BLACK =>
-          (pawnPosition.fileBackward().rankDown(), pawnPosition.fileForward().rankDown())
+      val capturingPositions = getCapturingPositions(pawn, pawnPosition)
 
       val foundMoves = PawnCaptureMoves().findMoves(pawnPosition, status)
       foundMoves shouldNot be(empty)
-      foundMoves.map(_.to) should contain atLeastOneOf (
-        capturingPositionLeft,
-        capturingPositionRight
-      )
+      foundMoves.map(_.to) should contain theSameElementsAs capturingPositions
+
       cleanBoard()
     })
+
+  private def getCapturingPositions(pawn: Pawn, position: Position): Set[Position] =
+    val followingPosition = pawn.team match
+      case Team.WHITE => position.rankUp()
+      case Team.BLACK => position.rankDown()
+
+    var capturingPositions: Set[Position] = Set.empty
+
+    if followingPosition.file != File.A
+    then capturingPositions = capturingPositions + followingPosition.fileBackward()
+    if followingPosition.file != File.H
+    then capturingPositions = capturingPositions + followingPosition.fileForward()
+
+    capturingPositions
